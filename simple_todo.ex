7f7d4ac42b69c2ed91b999c7b37733177adc19a0
Carlos Lopez <clopez@alertlogic.com>
2015-10-11 22:40:09 -0500

    Add entry with generated Id

--- simple_todo.ex
+++ simple_todo.ex
@@ -3,4 +3,13 @@ defmodule TodoList do
 
   def new, do: %TodoList{}
 
+  def add_entry(
+        %TodoList{entries: entries, auto_id: auto_id} = todo_list, entry
+      ) do
+    entry = Map.put(entry, :id, auto_id)
+    new_entries = HashDict.put(entries, auto_id, entry)
+
+    %TodoList.{todo_list | entries: new_entries, auto_id: auto_id + 1}
+  end
+
 end

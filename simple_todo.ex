ba49929fe02faebf41185e923b2efdf709d09af8
Carlos Lopez <clopez@alertlogic.com>
2015-10-11 20:09:46 -0500

    Build an abstraction for the todo-list

--- simple_todo.ex
+++ simple_todo.ex
@@ -1,24 +1,36 @@
+defmodule MultiDict do
+  def new, do: HashDict.new
+
+  def add(dic, key, value) do
+    HashDict.update(
+      dic,
+      key,
+      [value],
+      &[value | &1]
+    )
+  end
+
+  def get(dic, key) do
+    HashDict.get(dic, key, [])
+  end
+end
+
 defmodule TodoList do
 
-  def new, do: HashDict.new
+  def new, do: MultiDict.new
 
   @doc """
   add a new entry to the todo list
   """
   def add_entry(todo_list, data, title) do
-    HashDict.update(todo_list,
-                    date,
-                    [title],
-                    fn(titles) -> [title | titles] end
-    )
+    MultiDict.add(todo_list,data, title)
   end
 
   @doc """
   get all entries from the todo list
   """
   def entries(todo_list, date) do
-    HashDict.get(todo_list, date, [])
+    MultiDict.get(todo_list, date)
   end
-
 
 end

91d60201b495b3052516325816d0031277d42d9d
Carlos Lopez <clopez@alertlogic.com>
2015-10-11 19:39:08 -0500

    Add entry function

--- simple_todo.ex
+++ simple_todo.ex
@@ -1,3 +1,12 @@
 defmodule TodoList do
   def new, do: HashDict.new
+
+  def add_entry(todo_list, data, title) do
+    HashDict.update(todo_list,
+                    date,
+                    [title],
+                    fn(titles) -> [title | titles] end
+    )
+  end
+
 end

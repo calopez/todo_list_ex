b3ed895e970a9c6d41a5095097e5a25504538016
Carlos Lopez <clopez@alertlogic.com>
2015-10-12 08:04:27 -0500

    Filtering entries for a given date
    This function takes advantage of the fact that HashDict is an enumerable.
    When you use a HashDict instance with functions from Enum or Stream,
    then each HashDict ele- ment is treated in the form of {key, value}.
    
    The work so far it demonstrates how to construct and reason about
    higher-level types when working with stateless modules and immutable data

--- simple_todo.ex
+++ simple_todo.ex
@@ -1,4 +1,5 @@
 defmodule TodoList do
+
   defstruct auto_id: 1, entries: HashDict.new
 
   def new, do: %TodoList{}
@@ -9,7 +10,25 @@ defmodule TodoList do
     entry = Map.put(entry, :id, auto_id)
     new_entries = HashDict.put(entries, auto_id, entry)
 
-    %TodoList.{todo_list | entries: new_entries, auto_id: auto_id + 1}
+    %TodoList{todo_list | entries: new_entries, auto_id: auto_id + 1}
+  end
+
+  def entries(%TodoList{entries: entries}, date) do
+    entries
+    |> Stream.filter(fn({_, entry}) -> entry.date == date end)
+    |> Enum.map(fn({_, entry}) -> entry end) # only return values
   end
 
 end
+
+# todo_list = TodoList.new |>
+#   TodoList.add_entry(
+#     %{date: {2013, 12, 19}, title: "Dentist"}
+#   ) |>
+#   TodoList.add_entry(
+#     %{date: {2013, 12, 20}, title: "Shopping"}
+#   ) |>
+#   TodoList.add_entry(
+#     %{date: {2013, 12, 19}, title: "Movies"}
+
+# TodoList.entries(todo_list, {2013, 12, 19})

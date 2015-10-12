0c094729546c114c88c1d2c054693a492760e74d
Carlos Lopez <clopez@alertlogic.com>
2015-10-11 22:19:26 -0500

    Working with herarchical data
    When adding a new entry to the list, you’ll autogenerate its ID value,
    using incremen- tal integers for IDs. To implement this, you have to do -
    a couple of things:
    
    1) You need to transform the to-do list into a struct. This is because the
    to-do list now has to keep two pieces of information: the entries -
    collection and the ID value for the next entry.
    
    2) So far, when storing entries in a collection, you used the entry’s
    date as the key. You’ll change this and use the entry’s ID instead.
    This will make it possible to quickly insert, update, and delete -
    individual entries. Because you’ll now have exactly one value per each -
    key, you won’t need the MultiDict abstraction any- more.

--- simple_todo.ex
+++ simple_todo.ex
@@ -1,36 +1,6 @@
-defmodule MultiDict do
-  def new, do: HashDict.new
-
-  def add(dic, key, value) do
-    HashDict.update(
-      dic,
-      key,
-      [value],
-      &[value | &1]
-    )
-  end
-
-  def get(dic, key) do
-    HashDict.get(dic, key, [])
-  end
-end
-
 defmodule TodoList do
+  defstruct auto_id: 1, entries: HashDict.new
 
-  def new, do: MultiDict.new
-
-  @doc """
-  add a new entry to the todo list
-  """
-  def add_entry(todo_list, entry) do
-    MultiDict.add(todo_list, entry.date, entry)
-  end
-
-  @doc """
-  get all entries from the todo list
-  """
-  def entries(todo_list, date) do
-    MultiDict.get(todo_list, date)
-  end
+  def new, do: %TodoList{}
 
 end

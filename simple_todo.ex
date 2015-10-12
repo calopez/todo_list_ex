6bcfe258c23d317c3e5f35023b228c0004e48341
Carlos Lopez <clopez@alertlogic.com>
2015-10-11 19:53:25 -0500

    Get entries from dic

--- simple_todo.ex
+++ simple_todo.ex
@@ -1,6 +1,10 @@
 defmodule TodoList do
+
   def new, do: HashDict.new
 
+  @doc """
+  add a new entry to the todo list
+  """
   def add_entry(todo_list, data, title) do
     HashDict.update(todo_list,
                     date,
@@ -8,5 +12,13 @@ defmodule TodoList do
                     fn(titles) -> [title | titles] end
     )
   end
+
+  @doc """
+  get all entries from the todo list
+  """
+  def entries(todo_list, date) do
+    HashDict.get(todo_list, date, [])
+  end
+
 
 end
